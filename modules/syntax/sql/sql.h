f924002fb4de9f8c4dfdf90519c6534d76eeacc3
Zvonimir Rudinski <zvonimirurdinski@protonmail.ch>
2017-04-21 00:57:28 +0200

    add more sql

--- modules/syntax/sql/sql.h
+++ modules/syntax/sql/sql.h
@@ -4,21 +4,19 @@
 // MySQL, just the beginning
 char *SQL_extensions[] = {".sql" ,NULL};
 char *SQL_keywords[] = {
-	"create","table","insert","from","values","select",
+	"create","insert","from","values","select","alter","orderby",
+	"table~","database~","column~",
+	"asc@","desc@",
 	NULL
 };
 
 #define SQL_syntax { \
 	SQL_extensions, \
 	SQL_keywords, \
-	"#", \
 	"-- ", \
 	"/*", \
 	"*/", \
 	HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS \
 }
-
-
-
 
 #endif
