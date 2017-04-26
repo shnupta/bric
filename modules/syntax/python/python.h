61baee0ecd34fc6111acd4e73bb22ce9009c9ac0
Casey Williams <williamshoops96@gmail.com>
2017-04-26 21:20:38 +0100

    Update python.h

--- modules/syntax/python/python.h
+++ modules/syntax/python/python.h
@@ -3,8 +3,18 @@
 
 char *Python_extensions[] = {".py", NULL};
 char *Python_keywords[] = {
-	"and", "del", "from", "not", "while", "as", "elif", "or", "with", "assert", "yield", "import", "print", "exec", "in", "raise", "finally", "is", "return", "def", "for", "lambda",
-	"global|", "if|", "else|", "elif|", "global|", "class|", "break|", "continue|",
+	// data types
+	"str", "list", "tuple", "dict", "True", "False", "int", "None", "float",
+	// imports etc.
+	"import|", "from|",
+	//conditionals
+	"if~", "elif~", "continue~", "break~", "pass~", "try~", 
+	// return
+	"return#", "raise#", "is#", "in#",
+	// loops
+	"for@", "while@", "try@", "except@", "finally@",
+	// misc
+	"and^", "or^", "global^", "print^", "with^", "yield^", "not^", "class^", "as^",
 	NULL
 };
 
