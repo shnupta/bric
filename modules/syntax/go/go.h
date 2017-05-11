145381a5de3b5d4b88606a4937f70dac6540cf08
Patrick Chen <chen.j.patrick@gmail.com>
2017-05-11 16:26:54 +0200

    mini correction

--- modules/syntax/go/go.h
+++ modules/syntax/go/go.h
@@ -6,7 +6,7 @@ char *GO_extensions[] = {".go",NULL};
 char *GO_keywords[] = {
 	//types and misc
 	"bool", "byte", "int8", "float32","float64","uint64","complex128","complex64","uint32 ","var","null","true",
-	, "break", "import","package","false","const","float32","float64"," rune","string",uintptr,
+	, "break", "import","package","false","const","float32","float64"," rune","string","uintptr",
 	"func",
 
 	//conditionals
