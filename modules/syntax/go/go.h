761ff668524e45e7f97a5bdfe635806f324c5e9b
Patrick Chen <chen.j.patrick@gmail.com>
2017-05-11 16:08:12 +0200

    GoLang adding syntax

---
+++ modules/syntax/go/go.h
@@ -0,0 +1,34 @@
+#ifndef _SYNTAX_GO_H
+#define _SYNTAX_GO_H
+
+// GOLANG
+char *GO_extensions[] = {".go",NULL};
+char *GO_keywords[] = {
+	//types and misc
+	"bool", "byte", "int8", "float32","float64","uint64","complex128","complex64","uint32 ","var","null","true",
+	, "break", "import","package","false","const","float32","float64"," rune","string",uintptr,
+	"func",
+
+	//conditionals
+	"switch~", "if~", "else~","case~","default~",
+
+	//return
+	"return#", "goto#",
+
+
+	//loops
+	"for@",
+
+	NULL
+};
+
+#define GO_syntax { \
+	GO_extensions, \
+	GO_keywords, \
+	"//", \
+	"/*", \
+	"*/", \
+	HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS \
+}
+
+#endif
