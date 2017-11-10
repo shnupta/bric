4a4f17f722f8eacc56ddd973637466a61b6e4b34
Ivan Botero <ivan.botero@protonmail.ch>
2017-11-10 14:50:05 -0500

    disable_raw_mode() separated in a different header

--- src/handling.h
+++ src/handling.h
@@ -1,4 +1,11 @@
 #ifndef HANDLING_H
 #define HANDLING_H
 
+#include <unistd.h>
+#include <termios.h>
+
+#include "editor.h"
+
+void disable_raw_mode(int fd, struct termios *termios, struct editor_config *editor);
+
 #endif
