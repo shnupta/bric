97b39bb23bca86c07fae72a301b401a0b2c425a9
Martijn de Boer <github@sexybiggetje.nl>
2018-08-29 23:28:13 +0200

    Add syntax support for Haxe

---
+++ src/modules/syntax/haxe/haxe.h
@@ -0,0 +1,41 @@
+#ifndef _SYNTAX_HAXE_H
+#define _SYNTAX_HAXE_H
+
+// HAXE
+char *HAXE_extensions[] = {".hx",NULL};
+char *HAXE_keywords[] = {
+	//types and misc
+	"Bool", "Float", "Int", "true", "false", "String",
+	"null", "Void", "NaN", "Array",
+	"var", "trace", "Node", "TString", "TInt", "Class",
+
+	//conditionals
+	"switch~", "if~", "throw~", "else~", "case~", "try~", "catch~",
+	"Type~",
+
+	//return
+	"return#", "break#","continue#",
+
+	//adapters
+	"static^", "public^", "protected^", "private^", "class^", "extern^",
+	"function^", "import^", "using^", "typedef^", "inline^", "package^",
+
+	//loops
+	"for@", "while@", "do@", "in@", "...@",
+
+	// Should figure out metadata lixe @:keep and @:rtti selectors in haxe.
+	// https://haxe.org/manual/lf-metadata.html
+
+	NULL
+};
+
+#define HAXE_syntax { \
+	HAXE_extensions, \
+	HAXE_keywords, \
+	"//", \
+	"/*", \
+	"*/", \
+	HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS \
+}
+
+#endif
